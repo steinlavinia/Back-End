caa172e4b7ed2d464a9b344bc11e3ac0ccbc21d9
steinlavinia <steinlavinia@estudante.ifro.edu.br>
2024-09-26 11:17:42 -0400

    commit

--- Back-End II/apiUF/index.js
+++ Back-End II/apiUF/index.js
@@ -7,6 +7,27 @@ app.get('/ufs', (req, res) => {
     res.json(colecaoUf.colecaoUf);
 });
 
+app.get('/ufs/:iduf', (req, res) => { 
+    const idUF = parseInt(req.params.iduf);
+    let mensagemErro = '';
+    let uf;
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.colecaoUf.find(u => u.id === idUF); 
+        if (!uf) {
+            mensagemErro = 'UF não encontrada!';
+        }
+    } else {
+        mensagemErro = 'Requisição inválida!';
+    }
+    
+    if (uf) {
+        res.json(uf); 
+    } else {
+        res.status(404).json({'erro': mensagemErro})
+    }
+});
+
 app.listen(8080, () => {
     console.log('Servidor iniciado na porta 8080');
 });
