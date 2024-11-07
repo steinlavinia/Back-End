8b9b0b9d3b93ea3ba9addb48dceec57fd76501c6
steinlavinia <steinlavinia@estudante.ifro.edu.br>
2024-11-07 11:00:18 -0400

    commit

---
+++ Back-End II/API_IPCA/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { buscarIpca, buscarIpcaPorId, buscarIpcaPorAno } from './servicos/servico.js';
+
+const app = express();
+
+app.get('/historicoIPCA', (req, res) => {
+  const anoIpca = req.query.busca;
+  const resultado = anoIpca ? buscarIpcaPorAno(anoIpca) : buscarIpca();
+  if (resultado) {
+    res.json(resultado);
+  } else {
+    res.status(404).send({ "erro": "Nenhum histórico encontrado para o ano especificado" });
+  }
+});
+
+app.get('/historicoIPCA/:idIpca', (req, res) => {
+  const ipca = buscarIpcaPorId(req.params.idIpca);
+  if (ipca) {
+    res.json(ipca);
+  } else {
+    res.status(404).send({ "erro": "Elemento não encontrado" });
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Servidor iniciado na porta 8080');
+});
---
+++ Back-End II/API_IPCA/servicos/servico.js
@@ -0,0 +1,14 @@
+import historicoInflacao from "../dados/dados.js"
+
+export const buscarIpca = () => {
+    return historicoInflacao;
+};
+
+export const buscarIpcaPorId = (id) => {
+    const idIpca = parseInt(id);
+    return historicoInflacao.find(ipca => ipca.id === idIpca);
+};
+
+export const buscarIpcaPorAno = (anoIpca) => {
+    return historicoInflacao.filter(ipca => ipca.ano.getFullYear().includes(anoIpca.getFullYear()));
+};
