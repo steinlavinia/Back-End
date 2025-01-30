16a443eab674f0e4b0b4953c0398da1d842daec2
steinlavinia <stein.lavinia@estudante.ifro.edu.br>
2025-01-30 11:41:19 -0400

    commit

--- Back-End II/API_LIBERTADORES/index.js
+++ Back-End II/API_LIBERTADORES/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { retornaCampeonatos } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
 //import pool from './servico/conexao.js';
 
 const app = express();
@@ -7,6 +8,12 @@ const app = express();
 app.get('/campeonatos', async (req, res) => {
     const campeonatos = await retornaCampeonatos();
     res.json(campeonatos);
+})
+
+app.get('/campeonatos/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    const campeonato = await retornaCampeonatosID(id);
+    res.json(campeonato);
 })
 
 app.listen(9000, () => {
--- Back-End II/API_LIBERTADORES/servico/retornaCampeonatos_servico.js
+++ Back-End II/API_LIBERTADORES/servico/retornaCampeonatos_servico.js
@@ -9,3 +9,13 @@ export async function retornaCampeonatos() {
 
     return campeonatos;
 }
+
+export async function retornaCampeonatosID(id) {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query('SELECT id, vice, ano FROM campeonatos WHERE id = ' + id);
+    const campeonatos = campeonatos_tb[0];
+
+    conexao.release();
+
+    return campeonatos;
+}
