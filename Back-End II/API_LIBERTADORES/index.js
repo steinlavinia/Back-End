d19482e41d43452fbc190fd3d61729ce4d32a20a
steinlavinia <stein.lavinia@estudante.ifro.edu.br>
2025-01-31 11:37:09 -0400

    commit

--- Back-End II/API_LIBERTADORES/index.js
+++ Back-End II/API_LIBERTADORES/index.js
@@ -1,22 +1,37 @@
 import express from 'express';
-import { retornaCampeonatos } from './servico/retornaCampeonatos_servico.js';
-import { retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatos, retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatosID, retornaCampeonatosAno } from './servico/retornaCampeonatos_servico.js';
 //import pool from './servico/conexao.js';
 
 const app = express();
 
 app.get('/campeonatos', async (req, res) => {
-    const campeonatos = await retornaCampeonatos();
-    res.json(campeonatos);
+    let campeonatos;
+
+    const ano = req.query.ano;
+    const time = req.query.time;
+
+    if (typeof ano === 'undefined' && typeof time === 'undefined') {
+        campeonatos = await retornaCampeonatos();
+    } else if (typeof ano !== 'undefined') {
+        campeonatos = await retornaCampeonatosAno(parseInt(ano));
+    } else if (typeof time !== 'undefined') {
+        campeonatos = await retornaCampeonatosTime(time);
+    }
+
+    if (campeonatos.length > 0) {
+        res.json(campeonatos);
+    } else {
+        res.status(404).json({mensagem: 'Nenhum campeonato encontrado'});
+    }
 })
 
 app.get('/campeonatos/:id', async (req, res) => {
     const id = parseInt(req.params.id);
-    const campeonato = await retornaCampeonatosID(id);
+    const campeonato = await retornaCampeonatosID(id);              
     if (campeonato.length > 0) {
         res.json(campeonato);
-    }
-    else {
+    } else {
         res.status(404).json({mensagem: "Nenhum campeonato encontrado"});
     }
 });
--- Back-End II/API_LIBERTADORES/servico/retornaCampeonatos_servico.js
+++ Back-End II/API_LIBERTADORES/servico/retornaCampeonatos_servico.js
@@ -2,7 +2,7 @@ import pool from "./conexao.js";
 
 export async function retornaCampeonatos() {
     const conexao = await pool.getConnection();
-    const campeonatos_tb = await conexao.query('SELECT id, vice, ano FROM campeonatos');
+    const campeonatos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos');
     const campeonatos = campeonatos_tb[0];
 
     conexao.release();
@@ -12,7 +12,7 @@ export async function retornaCampeonatos() {
 
 export async function retornaCampeonatosID(id) {
     const conexao = await pool.getConnection();
-    const campeonatos_tb = await conexao.query('SELECT id, vice, ano FROM campeonatos WHERE id = ' + id);
+    const campeonatos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE id = ' + id);
     const campeonatos = campeonatos_tb[0];
 
     conexao.release();
@@ -24,6 +24,15 @@ export async function retornaCampeonatosAno(ano) {
     const conexao = await pool.getConnection();
     const campeonatos_tb = await conexao.query(
         'SELECT id, campeao, vice, ano FROM campeonatos WHERE ano = ' + ano);
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
+
+export async function retornaCampeonatosTime(time) {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query(
+        'SELECT id, campeao, vice, ano FROM campeonatos WHERE campeao = ' + time + '');
     const campeonatos = campeonatos_tb[0];
     conexao.release();
     return campeonatos;
