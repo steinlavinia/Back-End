d20bd294e366f3a4cee9c3ec81513996fff7a599
steinlavinia <stein.lavinia@estudante.ifro.edu.br>
2025-02-06 10:20:37 -0400

    commit

--- Back-End II/API_LIBERTADORES/servico/retornaCampeonatos_servico.js
+++ Back-End II/API_LIBERTADORES/servico/retornaCampeonatos_servico.js
@@ -10,32 +10,30 @@ export async function retornaCampeonatos() {
 
 export async function retornaCampeonatosID(id) {
     const conexao = await pool.getConnection();
-    const campeonatos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE id = ' + id);
-    const campeonatos = campeonatos_tb[0];
+    const query = 'SELECT id, campeao, vice, ano FROM campeonatos WHERE id = ' + id;
+    const campeonatos = executaQuery(conexao, query);
     conexao.release();
     return campeonatos;
 }
 
 export async function retornaCampeonatosAno(ano) {
     const conexao = await pool.getConnection();
-    const campeonatos_tb = await conexao.query(
-        'SELECT id, campeao, vice, ano FROM campeonatos WHERE ano = ' + ano);
-    const campeonatos = campeonatos_tb[0];
+    const query = 'SELECT id, campeao, vice, ano FROM campeonatos WHERE ano = ' + ano;
+    const campeonatos = executaQuery(conexao, query)
     conexao.release();
     return campeonatos;
 }
 
 export async function retornaCampeonatosTime(time) {
     const conexao = await pool.getConnection();
-    const campeonatos_tb = await conexao.query(
-        'SELECT id, campeao, vice, ano FROM campeonatos WHERE campeao like "%' + time + '%"');
-    const campeonatos = campeonatos_tb[0];
+    const query = 'SELECT id, campeao, vice, ano FROM campeonatos WHERE campeao like "%' + time + '%"';
+    const campeonatos = executaQuery(conexao, query);
     conexao.release();
     return campeonatos;
 }
 
 async function executaQuery (conexao, query) {
-    const resultado_query = await conexao.query(query);
+    const resultado_query = await conexao.execute(query);
     const resposta = resultado_query[0];
     return resposta;
 }
