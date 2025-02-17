481728c30ee5f978fd34648a8b3610659cfef272
steinlavinia <stein.lavinia@estudante.ifro.edu.br>
2025-02-17 08:18:46 -0400

    commit

--- Back-End II/API_LEADS/index.js
+++ Back-End II/API_LEADS/index.js
@@ -7,3 +7,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.post('/usuarios', async (req,res) => {
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const telefone = req.body.telefone;
+
+    await cadastroLeads(nome, email, telefone);
+
+    res.status(204).send({'Mensagem': 'Cadastro efetivado com sucesso!'});
+});
